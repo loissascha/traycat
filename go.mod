08af88e2a2a6b2eebb69613d8932083e82ebf073
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-07-16 08:37:53 +0200

    start project

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/loissascha/go-systray-test
+
+go 1.24.1
+
+require fyne.io/systray v1.11.0
+
+require (
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"fyne.io/systray"
+	"fyne.io/systray/example/icon"
+)
+
+func main() {
+	systray.Run(onReady, onExit)
+	fmt.Println("Finished app")
+}
+
+func onReady() {
+	systray.SetIcon(icon.Data)
+	systray.SetTitle("Awesome App")
+	systray.SetTooltip("Pretty awesome超级棒")
+	addQuitItem()
+}
+
+func addQuitItem() {
+	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+	// Sets the icon of a menu item.
+	// mQuit.SetIcon(icon.Data)
+	go func() {
+		for range mQuit.ClickedCh {
+			fmt.Println("Requesting quit")
+			systray.Quit()
+		}
+	}()
+}
+
+func onExit() {
+	now := time.Now()
+	fmt.Println("Exit at", now.String())
+}
