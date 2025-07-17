4659fb12ade980a3aaf2b14e28011f01a48dc7a0
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-07-17 08:26:30 +0200

    autostart script

---
+++ autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p ~/.config/autostart
+
+echo "[Desktop Entry]
+Type=Application
+Name=Cat
+Exec=~/.local/bin/traycat
+Comment=Shows a beautiful cat" > ~/.config/autostart/traycat.desktop
+
+echo "autostart added. Log out and back in to see it in action!"
