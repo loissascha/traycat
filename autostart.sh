00f76b868ebf71594d2d88382f498d133b2fe67a
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-07-17 08:36:53 +0200

    autostart scripot

--- autostart.sh
+++ autostart.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.config/autostart
 echo "[Desktop Entry]
 Type=Application
 Name=Cat
-Exec=~/.local/bin/traycat
+Exec=/home/$USER/.local/bin/traycat
 Comment=Shows a beautiful cat" > ~/.config/autostart/traycat.desktop
 
 echo "autostart added. Log out and back in to see it in action!"
